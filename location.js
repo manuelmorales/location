0c9541fe3855804214b8fdedac9e79763bfcbf38
Manuel Morales <manuelmorales@gmail.com>
2015-09-25 20:07:46 +0100

    Enter supertest

--- location.js
+++ location.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-	res.send('Hello World!');
+app.get('/ips/:ip', function (req, res) {
+	res.send(req.params.ip);
 });
 
-var server = app.listen(3000, function () {
-	var host = server.address().address;
-	var port = server.address().port;
-
-	console.log('Example app listening at http://%s:%s', host, port);
-});
+module.exports = app;
