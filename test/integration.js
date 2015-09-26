27d491d778ad376ee0e1702daa35df0f930fa37c
Manuel Morales <manuelmorales@gmail.com>
2015-09-26 12:51:13 +0100

    Separated tests

--- test/integration.js
+++ test/integration.js
@@ -4,11 +4,19 @@ var request = require('supertest');
 var app = require('../location.js');
 
 describe('GET /ips/:ip', function () {
-	it('returns the IP', function (done) {
-		request(app)
-		.get('/ips/127.0.0.1')
-		.expect(200)
-		.expect('127.0.0.1')
-		.end(done);
+	var path = '/ips/127.0.0.1';
+
+	describe('when successful', function () {
+		it('has status 200 OK', function () {
+			request(app)
+				.get(path)
+				.expect(200)
+		});
+
+		it('returns the IP', function () {
+			request(app)
+			.get(path)
+			.expect('127.0.0.1')
+		});
 	});
 });
