c42c45e992427f78a8bd44a2b1778261d32b9f22
Manuel Morales <manuelmorales@gmail.com>
2015-09-27 14:04:08 +0100

    grunt bootstrap

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,12 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-mocha-test');
+	grunt.loadNpmTasks('grunt-curl');
+	grunt.loadNpmTasks('grunt-shell');
 
 	grunt.registerTask('default', ['jshint', 'mochaTest']);
+	grunt.registerTask('test', ['mochaTest']);
+	grunt.registerTask('bootstrap', ['curl:geolite', 'shell:unzip_geolite']);
 
 	grunt.initConfig({
 		watch: {
@@ -13,9 +17,11 @@ module.exports = function (grunt) {
 				options: {spawn: false}
 			}
 		},
+
 		jshint: {
 			all: ['*.js', 'lib/*.js', 'test/*.js']
 		},
+
 		mochaTest: {
 			test: {
 				options: {
@@ -23,6 +29,17 @@ module.exports = function (grunt) {
 				},
 				src: ['test/*.js']
 			}
+		},
+
+		curl: {
+			geolite: {
+				dest: 'vendor/GeoLite2-Country.mmdb.gz',
+				src: 'http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz'
+			}
+		},
+
+		shell: {
+			unzip_geolite: {command: 'gunzip vendor/GeoLite2-Country.mmdb.gz'}
 		}
 	});
 };
--- README.md
+++ README.md
@@ -1,4 +1,51 @@
 # Location Microservice
 
 Tiny service for locating IP addresses geographically.
+Created with the purpose of learning Node.js.
+Relies on [MaxMind's database](http://dev.maxmind.com/geoip/geoip2/geolite2/).
 
+
+## Sample
+
+```js
+// GET /ips/8.8.8.8
+{
+  host: "8.8.8.8",
+  country: {
+    iso_code: "US",
+    geoname_id: 6252001,
+    name: "United States"
+  }
+}
+```
+
+
+## Getting started
+
+Installation:
+
+```bash
+git clone https://github.com/manuelmorales/location.git
+cd location
+npm install
+grunt bootstrap # Will download the database
+```
+
+Check that everything is in order:
+
+```bash
+grunt test
+```
+
+
+Run the server:
+
+```bash
+node app.js
+```
+
+
+## License
+
+Released under the MIT License.
+See the [LICENSE](LICENSE.txt) file for further details.
