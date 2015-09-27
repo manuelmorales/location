4b38cdf5003dfa84b372b1a176f8bfaec2c8ff87
Manuel Morales <manuelmorales@gmail.com>
2015-09-27 13:22:44 +0100

    grunt mochaTest

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,29 @@
 module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default', ['jshint']);
+	grunt.loadNpmTasks('grunt-mocha-test');
+
+	grunt.registerTask('default', ['jshint', 'mochaTest']);
+
 	grunt.initConfig({
-		jshint: {
-			all: ['*.js', 'lib/*.js', 'test/*.js']
-		},
 		watch: {
 			scripts: {
 				files: ['*.js', 'lib/*.js', 'test/*.js'],
-				tasks: ['jshint'],
+				tasks: ['default'],
 				options: {spawn: false}
+			}
+		},
+		jshint: {
+			all: ['*.js', 'lib/*.js', 'test/*.js']
+		},
+		mochaTest: {
+			test: {
+				options: {
+					reporter: 'spec',
+				},
+				src: ['test/*.js']
 			}
 		}
 	});
 };
+
