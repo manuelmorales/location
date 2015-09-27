c2a512a4b259ef1141ef0027e89619dc856fc98d
Manuel Morales <manuelmorales@gmail.com>
2015-09-27 13:08:28 +0100

    grunt watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,17 @@
 module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.registerTask('default', ['jshint']);
 	grunt.initConfig({
 		jshint: {
 			all: ['Gruntfile.js', '*.js', 'test/*.js']
+		},
+		watch: {
+			scripts: {
+				files: ['Gruntfile.js', '*.js', 'test/*.js'],
+				tasks: ['jshint'],
+				options: {spawn: false}
+			}
 		}
 	});
 };
